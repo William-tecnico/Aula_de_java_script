a17dece80d9b241f6133b0a3e788b6741b94be09
William_técnico <wil.dossantos@gmail.com>
2022-05-12 13:27:38 -0300

    tipos de variaveis no script

---
+++ js/aula dois variaveis/Variaveis e seus valores/script.js
@@ -0,0 +1,73 @@
+// tipos primitivos
+
+// Boolean
+/*var vDuf = false;
+console.log (typeof(vDuf));
+
+// number
+var numeroqualquer = 1;
+console.log (typeof(numeroqualquer));
+
+//String
+var nome = "diana";
+console.log (typeof(nome));
+
+// function
+var funcao = function() {};
+console.log (typeof(funcao));*/
+
+// como declarar
+/*var variavel;
+console.log (variavel);
+
+var variavelcorreto = "diana";
+variavelcorreto = "Martine"
+console.log (variavelcorreto);*/
+
+/*let variavel2 = "diana";
+variavel2 = "martine";
+console.log (variavel2);*/
+
+//constante (const)
+
+/*const constante = "diana";
+constante = "Matine"
+console.log (constante);*/
+
+/*var escopoglobal = 'global';
+console.log (escopoglobal);
+
+function escopolocal() {
+    let escopolocalinterno = 'local';
+    console.log (escopolocalinterno);
+}
+escopolocal()*/
+
+// valores aritiméticos
+
+//adicao
+
+/*var adição = 1 + 2;
+console.log(adição);
+
+// subtracao
+var subtracao = 3 - 1;
+console.log(subtracao);
+
+// multiplicacao
+var multiplicacao = 2 * 3;
+console.log(multiplicacao);
+
+//divisao real
+var divisaoreal = 6 / 3;
+console.log(divisaoreal);
+
+// divisao inteira ou resto da divisao
+var divisaointeria = 5 % 2;
+console.log(divisaointeria);
+
+// potenciacao
+var potenciacao = 2 ** 6;
+console.log(potenciacao); */
+
+

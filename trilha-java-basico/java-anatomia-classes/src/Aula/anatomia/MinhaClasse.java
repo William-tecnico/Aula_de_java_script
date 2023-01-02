cb900a851f0289aca62309015e5aff6810b6506b
William_técnico <wil.dossantos@gmail.com>
2023-01-02 13:23:19 -0300

    Aula antomia java

---
+++ trilha-java-basico/java-anatomia-classes/src/Aula/anatomia/MinhaClasse.java
@@ -0,0 +1,19 @@
+package Aula.anatomia;
+public class MinhaClasse {
+    
+public static void main(String[] args) {
+
+    String primeiroNome = "William";
+    String segundoNome = "dos Santos";
+
+    String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+    
+    System.out.println(nomeCompleto);
+}
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
+}
+    
+
+
+}
